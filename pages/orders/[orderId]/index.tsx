4554f36a317ad3d4038b8b6603c57a97e8d55b04
dasimems <isaacseun63@gmail.com>
2025-02-09 00:28:00 +0100

    update: started working on the order details page

--- pages/orders/[orderId]/index.tsx
+++ pages/orders/[orderId]/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const OrderDetails = () => {
   return (
     <PageLayout>
-      <SectionContainer></SectionContainer>
+      <SectionContainer>this</SectionContainer>
     </PageLayout>
   );
 };
